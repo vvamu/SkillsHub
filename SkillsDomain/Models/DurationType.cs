c4df26f7d08ff3b9d3da2befd0f757fc31764837
Bichun Nadezhda <nadezhda.bn.02@mail.ru>
2024-04-09 20:39:23 +0300

    efe2

--- SkillsDomain/Models/DurationType.cs
+++ SkillsDomain/Models/DurationType.cs
@@ -7,4 +7,7 @@ public class DurationType : BaseEntity
     public DurationType? Parent { get; set; }
     public string? Name { get; set; }
     public string? Value { get; set; }
+
+    public List<Group>? Groups { get; set; }
+    public List<PaymentCategory>? PaymentCategories { get; set; }
 }
--- SkillsDomain/Models/Location.cs
+++ SkillsDomain/Models/Location.cs
@@ -1,4 +1,6 @@
 ﻿using SkillsHub.Domain.BaseModels;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
 
 namespace SkillsHub.Domain.Models;
 
@@ -7,5 +9,16 @@ public class Location : BaseEntity
     public Location? Parent { get; set; }
     public Guid? ParentId { get; set; }
     public bool IsOffline { get; set; }
+    [NotMapped]
+    public string DisplayName
+    {
+        get 
+        {
+            var result = "";
+            if (IsOffline) result = "Offline -";
+            else result += "Online - ";
+            return result + Description;
+        }
+    }
     public string Description { get; set; }
 }
